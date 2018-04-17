02f481b1bea2f868f7d8c5e736feb2477560ddf2
Vincent Lucero <vincent@unicard.com.au>
2018-04-18 08:54:47 +1000

    Initial Commit

---
+++ demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demo
+//
+//  Created by Yngwie Reigh Vincent Lucero on 18/4/18.
+//  Copyright © 2018 Yngwie Reigh Vincent Lucero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
