66db04fe7474524507cd8a4569c27eb69e89eebb
Vincent Lucero <vincent@unicard.com.au>
2018-04-18 11:34:49 +1000

    Find the first position letter a is on

--- demo/Test1ViewController.swift
+++ demo/Test1ViewController.swift
@@ -9,22 +9,34 @@
 import UIKit
 
 class Test1ViewController: UIViewController {
-
+    
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var findButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
+    var indeces = [String.Index]()
+    var message = "Position: "
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func find(_ sender: Any) {
+        guard let index = findA(string: textView.text) else {
+            resultLabel.text = "\(message) N/A"
+            return
+        }
+        resultLabel.text = "\(message) \(index)"
+    }
+    
+    func findA(string: String) -> Int? {
+        let letter: Character = "a"
+        return string.index(of: letter)?.encodedOffset.advanced(by: 1)
     }
     
 }
